f37c636f24ae7df84f0654659d7202def368c568
pixelfehler1996 <checkercraft1996@gmail.com>
2016-11-15 09:15:06 +0100

    languages and formatting

--- app/src/main/java/com/example/laudien/inflationcalc/MainActivity.java
+++ app/src/main/java/com/example/laudien/inflationcalc/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.laudien.inflationcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -11,13 +13,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import static android.view.inputmethod.InputMethodManager.HIDE_NOT_ALWAYS;
 
-public class MainActivity extends AppCompatActivity implements TextView.OnEditorActionListener{
+public class MainActivity extends AppCompatActivity implements TextView.OnEditorActionListener, View.OnClickListener {
 
     EditText editText_numberToday, editText_inflation, editText_years;
     TextView textView_result, textView_answer, textView_result2;
     Button button_calc;
+    NumberFormat formatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +37,24 @@ public class MainActivity extends AppCompatActivity implements TextView.OnEditor
         textView_answer = (TextView) findViewById(R.id.textView_answer);
         textView_result2 = (TextView) findViewById(R.id.textView_result2);
         button_calc = (Button) findViewById(R.id.button_calc);
+        formatter = NumberFormat.getCurrencyInstance();
 
+        button_calc.setOnClickListener(this);
         editText_years.setOnEditorActionListener(this);
     }
 
-    public void calc(View view){
+    public void calc(){
         if(editText_inflation.getText().toString().trim().equals("") ||
                 editText_numberToday.getText().toString().trim().equals("") ||
                 editText_years.getText().toString().trim().equals("")){
-            Toast.makeText(getApplicationContext(), "Es wurde nicht alles ausgefüllt!", Toast.LENGTH_SHORT).show();
-            textView_result.setText("Fehler!");
+            Toast.makeText(getApplicationContext(), getString(R.string.not_all_filled), Toast.LENGTH_SHORT).show();
+            textView_result.setText(getString(R.string.error));
             textView_answer.setText("");
             textView_result2.setText("");
             return;
         }
         double numberToday = Double.valueOf(editText_numberToday.getText().toString());
-        long inflation = Long.valueOf(editText_inflation.getText().toString());
+        double inflation = Double.valueOf(editText_inflation.getText().toString());
         long years = Long.valueOf(editText_years.getText().toString());
         double result = numberToday;
 
@@ -54,9 +62,9 @@ public class MainActivity extends AppCompatActivity implements TextView.OnEditor
             result -= result * inflation/100;
         }
         result = Math.round(100.0 * result) / 100.0;
-        textView_answer.setText(numberToday + "€ in " + years + " Jahren sind heute");
-        textView_result.setText(String.valueOf(result) + "€");
-        textView_result2.setText("wert!");
+        textView_answer.setText(formatter.format(numberToday) + " in " + years + " " + getString(R.string.years_equals_today));
+        textView_result.setText(formatter.format(result));
+        textView_result2.setText(getString(R.string.wert));
     }
 
     @Override
@@ -68,5 +76,10 @@ public class MainActivity extends AppCompatActivity implements TextView.OnEditor
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        calc();
     }
 }
