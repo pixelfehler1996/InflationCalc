ae331cb89ad93fc592b0c8cf06e2996d6a662a33
pixelfehler1996 <checkercraft1996@gmail.com>
2016-11-16 08:37:38 +0100

    cleaned

--- app/src/main/java/com/example/laudien/inflationcalc/MainActivity.java
+++ app/src/main/java/com/example/laudien/inflationcalc/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.laudien.inflationcalc;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
